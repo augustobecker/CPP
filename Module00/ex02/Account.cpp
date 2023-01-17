e4057ac46e66b1c1d8e10e6011a20d23950f2432
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-01-17 17:24:19 +0000

    Implementação total das funções privadas | the log files match

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -49,10 +49,11 @@ void	Account::displayAccountsInfos( void )
 Account::Account( int initial_deposit )
 {
 	this->_accountIndex = _nbAccounts;
+	this->_nbAccounts++;
 	this->_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";";
 	this->_amount = initial_deposit;
-	this->_totalAmount = initial_deposit;
+	this->_totalAmount += initial_deposit;
 	std::cout << "amount:" << this->checkAmount() << ";";
 	std::cout << "created";
 	std::cout << std::endl;
@@ -73,10 +74,12 @@ void	Account::makeDeposit( int deposit )
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "p_amount:" << this->_amount << ";";
 	this->_amount += deposit;
+	this->_totalAmount += deposit;
 	this->_nbDeposits++;
+	this->_totalNbDeposits++;
 	std::cout << "deposit:" << deposit << ";";
 	std::cout << "amount:" << this->_amount << ";";
-	std::cout << "nb_deposits:" << this->getNbDeposits();
+	std::cout << "nb_deposits:" << this->_nbDeposits;
 	std::cout << std::endl;
 }
 
@@ -88,10 +91,12 @@ bool	Account::makeWithdrawal( int withdrawal )
 	if (withdrawal <= this->_amount)
 	{
 		this->_amount -= withdrawal;
+		this->_totalAmount -= withdrawal;
 		this->_nbWithdrawals++;
+		this->_totalNbWithdrawals++;
 		std::cout << "withdrawal:" << withdrawal << ";";
 		std::cout << "amount:" << this->_amount << ";";
-		std::cout << "nb_withdrawals:" << this->getNbWithdrawals();
+		std::cout << "nb_withdrawals:" << this->_nbWithdrawals;
 		std::cout << std::endl;
 		return (true);
 	}
@@ -113,8 +118,8 @@ void	Account::displayStatus( void ) const
 	this->_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
-	std::cout << "deposits:" << this->getNbDeposits() << ";";
-	std::cout << "withdrawals:" << this->getNbWithdrawals();
+	std::cout << "deposits:" << this->_nbDeposits<< ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals;
 	std::cout << std::endl;
 }
 
@@ -145,6 +150,14 @@ void	Account::_displayTimestamp( void )
 	std::cout <<  current_time->tm_sec;
 	std::cout << ']';
 	std::cout << ' ';
+}
+
+Account::Account( void )
+{
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed";
+	std::cout << std::endl;
 }
 
 // ************************************************************************** //
