d22c18ea448bc84155eead4c3b6d80d84e2b5ef6
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-02-09 15:40:26 -0300

    [Module 02 Ex00] Exercise main

--- Module02/ex00/main.cpp
+++ Module02/ex00/main.cpp
@@ -12,7 +12,16 @@
 
 #include "Fixed.hpp"
 
-int main ( int argc, char **argv )
+int main ( void )
 {
-    return (0);
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
 }
