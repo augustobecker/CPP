d5cdccbd19982f0d72d8d7b574ec2098337c85c3
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-02-09 15:31:05 -0300

    [Module02 ex00] Class Fixed Implementation

--- Module02/ex00/Fixed.cpp
+++ Module02/ex00/Fixed.cpp
@@ -11,3 +11,39 @@
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed( void )
+{
+    std::cout << "Default constructor called" << std::endl;
+    this->value = 0;
+}
+
+Fixed::~Fixed( void )
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &obj)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = obj;
+}
+
+Fixed &Fixed::operator=(const Fixed &obj)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->value = obj.value;
+    return (*this);
+}
+
+int		Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->value);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+    std::cout << "settRawBits member function called" << std::endl;
+    this->value = raw;
+}
