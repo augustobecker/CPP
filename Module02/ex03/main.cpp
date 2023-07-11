ac14c9cd5a10b7924532873349560442172f6b03
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-07-11 21:05:51 +0000

    [Module 02-03] Add tests to main

--- Module02/ex03/main.cpp
+++ Module02/ex03/main.cpp
@@ -60,10 +60,10 @@ int main (void)
 		return (0);
 	}
 
-	//should be true
-	if (!isPointInsideTriangle(
-		5, 5,   15, 15,   10, 2,
-		10, 8)
+	//should be false
+	if (isPointInsideTriangle(
+		0.543f, 2.223f,   10.121f, 1.2121f,   10.4243f, 6.212f,
+		10.53f, 8.91f)
 	)
 	{
 		bpsFailMessage();
@@ -72,8 +72,8 @@ int main (void)
 
 	//should be true
 	if (!isPointInsideTriangle(
-		5, 5,   15, 15,   10, 2,
-		10, 8)
+		5.5355f, 5.1212f,   15.1f, 15,   10.1f, 2.1f,
+		10.09f, 8)
 	)
 	{
 		bpsFailMessage();
@@ -82,8 +82,8 @@ int main (void)
 
 	//should be true
 	if (!isPointInsideTriangle(
-		5, 5,   15, 15,   10, 2,
-		10, 8)
+		0, 0,   0, 8,   10.2f, 0,
+		2.56f, 2)
 	)
 	{
 		bpsFailMessage();
