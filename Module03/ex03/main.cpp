da1dd18b861a65a27119458f239ca44ce494c87c
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-08-22 17:12:03 +0000

    [Module 03-03] Tests: Add new tests to main.cpp

--- Module03/ex03/main.cpp
+++ Module03/ex03/main.cpp
@@ -7,6 +7,7 @@
 void print( std::string message );
 
 void testDiamondTrapConstructor( std::string name );
+void testDiamondTrapParameters( std::string name );
 void testDiamondTrapCopy( void );
 void testDiamondTrapWhoAmI( std::string name );
 void testDiamondOtherMethods( std::string name );
@@ -15,12 +16,29 @@ int main (void)
 {
     testDiamondTrapConstructor("Cicero");
     print("_______________________________________________");
+	testDiamondTrapParameters("Marco Aurelio");
+	print("_______________________________________________");
     testDiamondTrapCopy();
     print("_______________________________________________");
     testDiamondTrapWhoAmI("Thorin");
     print("_______________________________________________");
     testDiamondOtherMethods("Becker");
     print("_______________________________________________");
+}
+
+void testDiamondTrapConstructor( std::string name )
+{
+    print("___________| DiamondTrap Test Constructor |__________");
+    DiamondTrap random(name);
+}
+
+
+void testDiamondTrapParameters( std::string name )
+{
+	print("___________| DiamondTrap Test Initial Values |__________");
+	DiamondTrap random(name);
+
+	random.displayData();
 }
 
 void testDiamondTrapCopy( void )
@@ -43,12 +61,6 @@ void testDiamondTrapWhoAmI( std::string name )
     print("___________| DiamondTrap Test Who Am I Method |__________");
     DiamondTrap random(name);
     random.whoAmI();
-}
-
-void testDiamondTrapConstructor( std::string name )
-{
-    print("___________| DiamondTrap Test Constructor |__________");
-    DiamondTrap random(name);
 }
 
 void testDiamondOtherMethods( std::string name )
