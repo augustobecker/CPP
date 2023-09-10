b029fe65bc1a8b5ee56b0441f307f686ecaf3cf2
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-09-10 14:59:37 +0000

    [Module 04-02] Add more tests to main

--- Module04/ex02/main.cpp
+++ Module04/ex02/main.cpp
@@ -5,10 +5,28 @@
 # include "Brain.hpp"
 
 void subjectTests( void );
+void brainTests( void );
+void deepCopyTestsDog( void );
+void deepCopyTestsCat( void );
+void deepCopyConstructorTestsDog( void );
+void deepCopyConstructorTestsCat( void );
+void subjectExtraTests( void );
 
 int main( void )
 {
     subjectTests();
+    std::cout << "__________________________________________" << std::endl;
+	brainTests();
+    std::cout << "__________________________________________" << std::endl;
+    deepCopyTestsDog();
+    std::cout << "__________________________________________" << std::endl;
+    deepCopyTestsCat();
+    std::cout << "__________________________________________" << std::endl;
+    deepCopyConstructorTestsDog();
+    std::cout << "__________________________________________" << std::endl;
+    deepCopyConstructorTestsCat();
+    std::cout << "__________________________________________" << std::endl;
+	subjectExtraTests();
     std::cout << "__________________________________________" << std::endl;
 }
 
@@ -28,6 +46,121 @@ void subjectTests( void )
     //delete meta;
     delete j;
     delete i;
+
+    return ;
+}
+
+void brainTests( void )
+{
+	std::cout << "_________| Brain Tests |__________" << std::endl;
+
+    Dog michael;
+    Cat douglas;
+
+    std::cout << "Dog has an idea: " << michael.getIdea() << std::endl;
+    std::cout << "Cat has an idea: " << douglas.getIdea() << std::endl;
+}
+
+void deepCopyTestsDog( void )
+{
+    std::cout << "_________| Deep Copy Tests Dog |__________" << std::endl;
+
+    Dog michael;
+    Dog douglas;
+
+    std::cout << "||||||||||||| Michael Ideas |||||||||||||" << std::endl;
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Douglas Ideas |||||||||||||" << std::endl;
+    douglas.displayIdeas();
+    std::cout << "||||||||||||| Michael New Inverted Ideas |||||||||||||" << std::endl;
+    michael.invertIdeas();
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Deep Copying Michael to Douglas |||||||||||||" << std::endl;
+    douglas = michael;
+    std::cout << "||||||||||||| Douglas Ideas after DeepCopy |||||||||||||" << std::endl;
+    douglas.displayIdeas();
+    std::cout << "||||||||||||||||||||||||||" << std::endl;
+}
+
+void deepCopyTestsCat( void )
+{
+    std::cout << "_________| Deep Copy Tests Cat |__________" << std::endl;
+
+    Cat michael;
+    Cat douglas;
+
+    std::cout << "||||||||||||| Michael Ideas |||||||||||||" << std::endl;
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Douglas Ideas |||||||||||||" << std::endl;
+    douglas.displayIdeas();
+    std::cout << "||||||||||||| Michael New Inverted Ideas |||||||||||||" << std::endl;
+    michael.invertIdeas();
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Deep Copying Michael to Douglas |||||||||||||" << std::endl;
+    douglas = michael;
+    std::cout << "||||||||||||| Douglas Ideas after DeepCopy |||||||||||||" << std::endl;
+    douglas.displayIdeas();
+    std::cout << "||||||||||||||||||||||||||" << std::endl;
+}
+
+void deepCopyConstructorTestsDog( void )
+{
+    std::cout << "_________| Deep Copy Constructor Tests Dog |__________" << std::endl;
+
+    Dog michael;
+
+    std::cout << "||||||||||||| Michael Ideas |||||||||||||" << std::endl;
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Michael New Inverted Ideas |||||||||||||" << std::endl;
+    michael.invertIdeas();
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Deep Copy Constructor Michael to Douglas |||||||||||||" << std::endl;
+    Dog douglas = michael;
+    std::cout << "||||||||||||| Douglas Ideas after DeepCopy |||||||||||||" << std::endl;
+    douglas.displayIdeas();
+    std::cout << "||||||||||||||||||||||||||" << std::endl;
+}
+
+void deepCopyConstructorTestsCat( void )
+{
+    std::cout << "_________| Deep Copy Constructor Tests Cat |__________" << std::endl;
+
+    Cat michael;
+
+    std::cout << "||||||||||||| Michael Ideas |||||||||||||" << std::endl;
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Michael New Inverted Ideas |||||||||||||" << std::endl;
+    michael.invertIdeas();
+    michael.displayIdeas();
+    std::cout << "||||||||||||| Deep Copy Constructor Michael to Douglas |||||||||||||" << std::endl;
+    Cat douglas = michael;
+    std::cout << "||||||||||||| Douglas Ideas after DeepCopy |||||||||||||" << std::endl;
+    douglas.displayIdeas();
+    std::cout << "||||||||||||||||||||||||||" << std::endl;
+}
+
+void subjectExtraTests( void )
+{
+	std::cout << "_________| Subject Extra Tests |__________" << std::endl;
+
+    Animal *animals[10];
+
+    for (int i = 0; i < 10; ++i) 
+    {
+        if (i % 2 == 0)
+            animals[i] = new Cat();
+        else
+            animals[i] = new Dog();
+        std::cout << "+++++++++++++++" << std::endl;
+    }
+
+    std::cout << "||||||||||||| Destructor |||||||||||||" << std::endl;
+
+    for (int i = 0; i < 10; ++i) 
+    {
+        delete animals[i];
+        std::cout << "+++++++++++++++" << std::endl;
+    }
 
     return ;
 }
