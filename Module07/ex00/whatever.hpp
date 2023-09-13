755ecf8a56bba035ec7156f4488af301f8cc614d
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-09-13 20:16:53 +0000

    [Module 07-00] Templates min, max and swap Implemented

--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -1,6 +1,39 @@
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
-# include <iostream>
+#include <iostream>
+#include <string>
+
+template <typename T>
+T const& min(T const &x, T const &y)
+{
+    return ((x <= y) ? x : y);
+}
+
+template <typename T>
+T const& max(T const &x, T const &y)
+{
+    return ((x >= y) ? x : y);
+}
+
+template <typename T>
+void swap(T &x, T &y)
+{
+    T temp = x;
+    x = y;
+    y = temp;
+}
+
+template <>
+std::string const& min(std::string const &x, std::string const &y)
+{
+    return ((x.compare(y) <= 0) ? x : y);
+}
+
+template <>
+std::string const& max(std::string const &x, std::string const &y)
+{
+    return ((x.compare(y) >= 0) ? x : y);
+}
 
 #endif
