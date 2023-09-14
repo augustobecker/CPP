78d28c8d502e7b7112a5c81e505d8d7963f9c96b
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-09-14 03:15:13 +0000

    [Module 08-00] Initial Strucure

---
+++ Module08/ex01/easyfind.hpp
@@ -0,0 +1,12 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+T const *easyfind(T const *array, T const toFind)
+{
+    
+}
+
+#endif
---
+++ Module08/ex01/main.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+# include "easyfind.hpp"
+
+int main(void)
+{
+    return 0;
+}
