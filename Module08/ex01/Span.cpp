c432ca31fcb41c27378e0b3c4b4fbe78c6da7973
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2023-09-19 14:37:59 +0000

    [Module 08-01] Fix method longest/shortest span

--- Module08/ex01/Span.cpp
+++ Module08/ex01/Span.cpp
@@ -14,65 +14,69 @@ Span::~Span( void )
 
 Span::Span(const Span &obj) : _maxSize(obj._maxSize)
 {
-    *this = obj;
+	*this = obj;
 }
 
 Span& Span::operator=(const Span &toCopyFrom)
 {
-    unsigned int copySize = std::min(
-        static_cast<unsigned int>(toCopyFrom._elements.size()),
-        this->_maxSize);
+	unsigned int copySize = std::min(
+		static_cast<unsigned int>(toCopyFrom._elements.size()),
+		this->_maxSize);
 
-    if (this != &toCopyFrom)
-    {
-        this->_elements.clear();
-        this->_elements.insert(
-            this->_elements.end(), 
-            toCopyFrom._elements.begin(), 
-            toCopyFrom._elements.begin() + copySize);
-    }
-    return (*this);
+	if (this != &toCopyFrom)
+	{
+		this->_elements.clear();
+		this->_elements.insert(
+			this->_elements.end(), 
+			toCopyFrom._elements.begin(), 
+			toCopyFrom._elements.begin() + copySize);
+	}
+	return (*this);
 }
 
 void Span::addNumber( const int &newNbr )
 {
-    if (_elements.size() >= _maxSize)
-        throw std::overflow_error("Reached Max capacity of Vector");
-    _elements.push_back(newNbr);
+	if (_elements.size() >= _maxSize)
+		throw std::overflow_error("Reached Max capacity of Vector");
+	_elements.push_back(newNbr);
 }
 
 unsigned int absoluteValue (int value)
 {
-    return (std::abs(value));
+	return (std::abs(value));
 }
 
 unsigned int    Span::shortestSpan( void ) const
 {
-    if (_elements.size() <= 1)
-        throw std::out_of_range("Insufficient values to calculate Span");
+	if (_elements.size() <= 1)
+		throw std::out_of_range("Insufficient values to calculate Span");
 
-    std::vector<int> differences(_elements.size());
+	std::vector<int> differences(_elements.size());
 
-    std::adjacent_difference(_elements.begin(), _elements.end(), differences.begin());
-    std::transform(differences.begin(), differences.end(), differences.begin(), absoluteValue);
+	std::adjacent_difference(_elements.begin(), _elements.end(), differences.begin());
+	std::transform(differences.begin(), differences.end(), differences.begin(), absoluteValue);
 
-    return (*std::min_element(differences.begin(), differences.end()));
+	return (*std::min_element(differences.begin() + 1, differences.end()));
 
 }
 
 unsigned int    Span::longestSpan( void ) const
 {
-    if (_elements.size() <= 1)
-        throw std::out_of_range("Insufficient values to calculate Span");
-    return (
-        *std::max_element(_elements.begin(), _elements.end()) -
-        *std::min_element(_elements.begin(), _elements.end()));
+	if (_elements.size() <= 1)
+		throw std::out_of_range("Insufficient values to calculate Span");
+
+	std::vector<int> differences(_elements.size());
+
+	std::adjacent_difference(_elements.begin(), _elements.end(), differences.begin());
+	std::transform(differences.begin(), differences.end(), differences.begin(), absoluteValue);
+
+	return (*std::max_element(differences.begin() + 1, differences.end()));
 }
 
 void Span::displayElem( void ) const
 {
-    std::cout << "Span: ";
-    for (int i = 0; i < (int) _elements.size(); i++)
-        std::cout << "[" << _elements.at(i) << "] ";
-    std::cout << std::endl;
+	std::cout << "Span: ";
+	for (int i = 0; i < (int) _elements.size(); i++)
+		std::cout << "[" << _elements.at(i) << "] ";
+	std::cout << std::endl;
 }
